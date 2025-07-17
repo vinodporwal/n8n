90df410d28b1f44bb16862a9b2d646ed621d9280
vinodporwal <74370544+vinodporwal@users.noreply.github.com>
2025-07-17 23:27:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ nohup "$NODE" "$N8N" start > "$DIR/n8n.log" 2>&1 &
 
 # Optionally: Install as systemd service (if root)
 if [ "$EUID" -eq 0 ]; then
-    cp "$DIR/service-config/n8n.service" /etc/systemd/system/n8n.service
+    cp "$DIR/n8n.service_linux" /etc/systemd/system/n8n.service
     systemctl daemon-reexec
     systemctl enable n8n
     systemctl start n8n
